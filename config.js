0d2a584d8fd30f07abc9cc81374b74655d3afa78
POdDA-MD-WABOT <poddamdwhatsappbot@gmail.com>
2026-07-02 17:56:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "H1QFyZjT#1SDKo_k2PcKOrH3SMKlex6mYS7TWEwKc11W6CX7v2xU",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/POdDA-MD-WABOT/PODDA-MD-TOOLS/blob/main/20250706_195750.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PODDA-MD Is Alive Now😍 /n.menu to get all commands 👉👈*",
 BOT_OWNER: '94717024813',  // Replace with the owner's phone number
 
